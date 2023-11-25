9d5f2f9c8c6366b6944e403cb23b9c5ae3162f9b
Itorophilip1998 <itorophilip1998@gmail.com>
2023-11-25 13:31:11 +0100

    dev

--- app/Models/ListingVideo.php
+++ app/Models/ListingVideo.php
@@ -11,4 +11,15 @@ class ListingVideo extends Model
         'youtube_video_id'
     ];
 
+    public function toArray()
+    {
+        $attributes = parent::toArray();
+
+        // Add the 'user_photo' attribute to the array
+        $attributes['video_url'] =
+        $this->youtube_video_id ? "https://www.youtube.com/embed/" . $this->youtube_video_id :  null;
+
+        return $attributes;
+    }
+
 }
