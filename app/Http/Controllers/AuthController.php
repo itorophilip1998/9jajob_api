55394d650e397c101e19a7087cb0d17f4cf431f7
Itorophilip1998 <itorophilip1998@gmail.com>
2023-12-21 15:11:24 +0100

    dev

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -226,7 +226,7 @@ class AuthController extends Controller
         return response()->json([
             'access_token' => $token,
             'token_type' => 'bearer',
-            'expires_in' => 999999999999999999,
+            'expires_in' => auth()->factory()->getTTL() * 60,
             "message" => $message
         ], 200);
     }
