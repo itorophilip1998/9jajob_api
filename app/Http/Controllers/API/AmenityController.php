69e0c1f028f30c6e2286ee85d410c0c8fcf5c025
Itorophilip1998 <itorophilip1998@gmail.com>
2023-12-25 02:13:43 +0100

    dev

--- app/Http/Controllers/API/AmenityController.php
+++ app/Http/Controllers/API/AmenityController.php
@@ -1,15 +1,15 @@
 <?php
 namespace App\Http\Controllers\API;
- 
+
 use App\Http\Controllers\Controller;
-use App\Models\Amenity; 
+use App\Models\Amenity;
 
 class AmenityController extends Controller
 {
- 
+
     public function index()
     {
-        $amenity = Amenity::all();
+        $amenity = Amenity::orderBy('amenity_name')->get();
         return response()->json($amenity, 200);
     }
 }
