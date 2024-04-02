50ce62db49a190b6d24cd13f085a99a1c5465ecf
Itorophilip1998 <itorophilip1998@gmail.com>
2024-04-02 11:08:08 +0100

    dev

--- resources/views/mail/bookingMail.blade.php
+++ resources/views/mail/bookingMail.blade.php
@@ -6,14 +6,15 @@
         <h2>Dear {{  $item["user"] }},</h2>
 
 <p>{{ $item['mailInfo'] }}</p>
-<p>
 
+
+{{-- <p>
  <h4>Booking Details:</h4>
 - Date Booked: {{ $item?['booking']?['date'] }} <br/>
 - Time:{{ $item?['booking']?['time'] }}  <br/>
 - Location: {{ $item?['booking']?['location'] }}  <br/>
 - Phone: {{ $item?['booking']?['phone'] }} <br/>
-</p>
+</p> --}}
 
 
 <p>
