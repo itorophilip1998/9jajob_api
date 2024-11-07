4092fa3c1e60acb453ef8e071c5ec9ed44383c1f
Itorophilip1998 <itorophilip1998@gmail.com>
2024-11-07 11:01:59 +0100

    Update

--- resources/views/mail/layouts/mail_header.blade.php
+++ resources/views/mail/layouts/mail_header.blade.php
@@ -1,3 +1,3 @@
 <header>
-    <img src="https://i.ibb.co/w0HqPC0/Group-93.png" alt="Company Logo" class="logo">
+    <img src="https://i.ibb.co/Gs9RYyX/sabifix-124621.png" alt="Company Logo" class="logo">
 </header>
