bdb9086fa333b3a2a3b746e85e364fafc5e0d05e
Itorophilip1998 <itorophilip1998@gmail.com>
2024-11-09 10:17:08 +0100

    Update

--- app/Http/Controllers/VerificationController.php
+++ app/Http/Controllers/VerificationController.php
@@ -24,114 +24,118 @@ class VerificationController extends Controller
     public function create()
     {
 
-        $extra_details = DB::table("extra_details")->first();
-        $amount = $extra_details?->verification_amount;
-    
-        $balance = (new Balance)->check($amount);
+    try {
+            $extra_details = DB::table("extra_details")->first();
+            $amount = $extra_details?->verification_amount;
 
-        // dump($extra_details?->system_payment_mode, $balance, $amount);
-        if ($balance <= $amount)
-            return response()->json(['error' => 'Insufficient balance'], 422);
+            $balance = (new Balance)->check($amount);
+
+            // dump($extra_details?->system_payment_mode, $balance, $amount);
+            if ($balance <= $amount)
+                return response()->json(['error' => 'Insufficient balance'], 422);
 
 
-        $validator = Validator::make(request()->all(), [
-            'listing_id' => 'required',
-            'id_card_front' => 'required|file',
-        ]);
-        if ($validator->fails()) {
-            return response()->json(['error' => $validator->messages()], 422);
-        }
+            $validator = Validator::make(request()->all(), [
+                'listing_id' => 'required',
+                'id_card_front' => 'required|file',
+            ]);
+            if ($validator->fails()) {
+                return response()->json(['error' => $validator->messages()], 422);
+            }
 
-        // check balance
-        $totalBalance = Transactions::where(['user_id' => auth()->user()->id])->get()->sum('amount');
-        if ($totalBalance < 1000) {
-            return response()->json(['message' => 'insufficient fund'], 200);
-        }
-        $req = request()->all();
+            // check balance
+            $totalBalance = Transactions::where(['user_id' => auth()->user()->id])->get()->sum('amount');
+            if ($totalBalance < 1000) {
+                return response()->json(['message' => 'insufficient fund'], 200);
+            }
+            $req = request()->all();
 
-        if (request()->hasFile('id_card_front')) {
-            $item = request()->file('id_card_front');
-            $id_card_front =  (new Upload)->image($item, 'uploads/verifications/');
-            $req['id_card_front'] = $id_card_front;
-        }
-        if (request()->hasFile('id_card_back')) {
-            $item = request()->file('id_card_back');
-            $id_card_back =  (new Upload)->image($item, 'uploads/verifications/');
-            $req['id_card_back'] = $id_card_back;
-        }
-        if (request()->hasFile('proof_address')) {
-            $item =   request()->file('proof_address');
-            $proof_address =  (new Upload)->document($item, 'uploads/verifications/');
-            $req['proof_address'] = $proof_address;
-        }
-        if (request()->hasFile('cac_document')) {
-            $item = request()->file('cac_document');
-            $cac_document =  (new Upload)->document($item, 'uploads/verifications/');
-            $req['cac_document'] = $cac_document;
-        }
+            if (request()->hasFile('id_card_front')) {
+                $item = request()->file('id_card_front');
+                $id_card_front =  (new Upload)->image($item, 'uploads/verifications/');
+                $req['id_card_front'] = $id_card_front;
+            }
+            if (request()->hasFile('id_card_back')) {
+                $item = request()->file('id_card_back');
+                $id_card_back =  (new Upload)->image($item, 'uploads/verifications/');
+                $req['id_card_back'] = $id_card_back;
+            }
+            if (request()->hasFile('proof_address')) {
+                $item =   request()->file('proof_address');
+                $proof_address =  (new Upload)->document($item, 'uploads/verifications/');
+                $req['proof_address'] = $proof_address;
+            }
+            if (request()->hasFile('cac_document')) {
+                $item = request()->file('cac_document');
+                $cac_document =  (new Upload)->document($item, 'uploads/verifications/');
+                $req['cac_document'] = $cac_document;
+            }
 
-        if (request()->hasFile('skill_certificate')) {
-            $item = request()->file('skill_certificate');
-            $skill_certificate =  (new Upload)->document($item, 'uploads/verifications/');
-            $req['skill_certificate'] = $skill_certificate;
-        }
-        if (is_array(request()->services)) {
-            $req['services'] = json_encode(request()->services);
-        }
+            if (request()->hasFile('skill_certificate')) {
+                $item = request()->file('skill_certificate');
+                $skill_certificate =  (new Upload)->document($item, 'uploads/verifications/');
+                $req['skill_certificate'] = $skill_certificate;
+            }
+            if (is_array(request()->services)) {
+                $req['services'] = json_encode(request()->services);
+            }
 
-        $isVerified = Verification::where('listing_id', $req['listing_id'])->first();
+            $isVerified = Verification::where('listing_id', $req['listing_id'])->first();
 
-        if (isset($isVerified) && $isVerified->status == 'pending') return response()->json(['message' => 'There is an existing verification!'], 200);
+            if (isset($isVerified) && $isVerified->status == 'pending') return response()->json(['message' => 'There is an existing verification!'], 200);
 
-        else if (isset($isVerified) &&  $isVerified->status != 'completed') return response()->json(['message' => 'Verification Completed Already!'], 200);
+            else if (isset($isVerified) &&  $isVerified->status != 'completed') return response()->json(['message' => 'Verification Completed Already!'], 200);
 
-        if (!request()->reg_number || request()->reg_number === NULL)  $req['reg_number'] = "N/A";
+            if (!request()->reg_number || request()->reg_number === NULL)  $req['reg_number'] = "N/A";
 
-        Verification::create($req);
-        // send transaction
-        $ref_number = Str::random(10);
-        $transaction = [
-            'user_id' => auth()->user()->id,
-            'type' => 'debit',
-            'status' => 'success', //debit, credit
-            'ref_number' => $ref_number,
-            'trans_id' => $ref_number,
-            'amount' => 1000,
-            'description' => "Verification from " . auth()->user()->name,
-            'purpose' => 'verification', //verification ,packages, top-up, withdrawal,referrals, boost]
-
-        ];
-        Transactions::create($transaction);
-
-        // send invioce
-        $item = [
-            "invoiceNumber" => rand(1111, 9999),
-            "invoiceDate" => Carbon::now()->format("d M, Y"),
-            "user" => auth()->user()->name,
-            "purpose" => $transaction["purpose"],
-            "status" => $transaction["status"],
-            "ref_number" => $transaction["ref_number"],
-            "amount" => $transaction["amount"],
-        ];
-
-        Notification::create(
-            [
-                'message' => $transaction['description'],
+            Verification::create($req);
+            // send transaction
+            $ref_number = Str::random(10);
+            $transaction = [
                 'user_id' => auth()->user()->id,
-                'title' => 'Verification',
-                'status' => 'unread'
-            ]
-        );
+                'type' => 'debit',
+                'status' => 'success', //debit, credit
+                'ref_number' => $ref_number,
+                'trans_id' => $ref_number,
+                'amount' => 1000,
+                'description' => "Verification from " . auth()->user()->name,
+                'purpose' => 'verification', //verification ,packages, top-up, withdrawal,referrals, boost]
 
-        DB::table('admin_notifications')->insert(
-            [
-                'description' =>  $transaction['description'],
-                'title' => "Verification",
-                'status' => 'unread',
-                'created_at' => Carbon::now()
-            ]
-        );
-        return response()->json(['message' => 'Verification In Progress!!!'], 200);
+            ];
+            Transactions::create($transaction);
+
+            // send invioce
+            $item = [
+                "invoiceNumber" => rand(1111, 9999),
+                "invoiceDate" => Carbon::now()->format("d M, Y"),
+                "user" => auth()->user()->name,
+                "purpose" => $transaction["purpose"],
+                "status" => $transaction["status"],
+                "ref_number" => $transaction["ref_number"],
+                "amount" => $transaction["amount"],
+            ];
+
+            Notification::create(
+                [
+                    'message' => $transaction['description'],
+                    'user_id' => auth()->user()->id,
+                    'title' => 'Verification',
+                    'status' => 'unread'
+                ]
+            );
+
+            DB::table('admin_notifications')->insert(
+                [
+                    'description' =>  $transaction['description'],
+                    'title' => "Verification",
+                    'status' => 'unread',
+                    'created_at' => Carbon::now()
+                ]
+            );
+            return response()->json(['message' => 'Verification In Progress!!!'], 200);
+    } catch (\Throwable $th) {
+        throw $th;
+    }
     }
 
     public function listOfVerifications()
