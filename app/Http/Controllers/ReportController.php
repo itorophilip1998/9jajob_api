b4c8e1074339e49efd2ced05255bd6b1b3afb25e
Itorophilip1998 <itorophilip1998@gmail.com>
2024-11-09 15:39:51 +0100

    Update

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -16,24 +16,28 @@ class ReportController extends Controller
 
     public function create()
     {
-        $validator = Validator::make(request()->all(), [
-            'listing_id' => 'required',
-            'report' => 'required',
-        ]);
-        if ($validator->fails()) {
-            return response()->json(['error' => $validator->messages()], 422);
+        try {
+            $validator = Validator::make(request()->all(), [
+                'listing_id' => 'required',
+                'report' => 'required',
+            ]);
+            if ($validator->fails()) {
+                return response()->json(['error' => $validator->messages()], 422);
+            }
+            $req = request()->all();
+            $req['reporter_id'] = auth()->user()->id;
+            $report = Report::create($req);
+            $listingName = $report?->user?->name;
+            $subject = "Reporting a user";
+            $message = request()->report . "<br/> <b>Listing:</b> $listingName";
+            $message = str_replace('[[visitor_name]]', request()->name, $message);
+            $message = str_replace('[[visitor_email]]', request()->email, $message);
+            $message = str_replace('[[visitor_phone]]', request()->phone, $message);
+            $message = str_replace('[[visitor_message]]', request()->report, $message);
+            Mail::to('support@sabifix.biz')->queue(new ContactPageMessage($subject, $message));
+            return response()->json(['message' => 'Success!!'], 200);
+        } catch (\Throwable $th) {
+            throw $th;
         }
-        $req = request()->all();
-        $req['reporter_id'] = auth()->user()->id;
-        $report = Report::create($req);
-        $listingName=$report?->user?->name;
-        $subject = "Reporting a user";
-        $message = request()->report."<br/> <b>Listing:</b> $listingName";
-        $message = str_replace('[[visitor_name]]', request()->name, $message);
-        $message = str_replace('[[visitor_email]]', request()->email, $message);
-        $message = str_replace('[[visitor_phone]]', request()->phone, $message);
-        $message = str_replace('[[visitor_message]]', request()->report, $message);
-        Mail::to('support@sabifix.biz')->queue(new ContactPageMessage($subject, $message));
-        return response()->json(['message' => 'Success!!'], 200);
     }
 }
