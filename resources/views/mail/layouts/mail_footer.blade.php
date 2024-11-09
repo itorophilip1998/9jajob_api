546a9601d5272a895f8b590a6ef23c46005a0ea6
Itorophilip1998 <itorophilip1998@gmail.com>
2024-11-09 21:20:39 +0100

    Updated

--- resources/views/mail/layouts/mail_footer.blade.php
+++ resources/views/mail/layouts/mail_footer.blade.php
@@ -1,6 +1,6 @@
 <footer>
    <p> For any inquiries or assistance, you can reach us at: <br/>
-    Phone: +234 813 214 2030 <br/>
+    Phone: +234 915 198 7637 <br/>
      Email: support@Sabifix.biz  <br/><br/>
     Thank you for choosing 9jajob.
 </p>
